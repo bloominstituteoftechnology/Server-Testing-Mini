106ca2ce7ba9d962f5cab4c5fae1f0d19620daca
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-31 15:04:56 -0500

    Everything setup to go on

--- server-testing-mini/server.js
+++ server-testing-mini/server.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const port = 3333;
+
+server.use(morgan('combined'));
+server.use(bodyParser.json());
+
+server.listen(port, () => {
+  console.log(`Server is running on port:${port}`);
+});
+
+module.exports = server;
