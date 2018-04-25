e3259d3a23f42ad2f80bf8c07b6ae2b36844c76d
Edward Manda <lilkups@yahoo.com>
2018-04-25 17:46:55 -0500

    setting up the test environment, wip

--- routes.test.js
+++ routes.test.js
@@ -1,1 +1,24 @@
-console.log('HHHHHHHHHHH');
+// connect to a test dummy database server
+const mongoose = require('mongoose');
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+
+const config = require('./api/config');
+const server = require('./server');
+const Band = require('./bands/band');
+
+const expect = chai.expect;
+
+mongoose.connect(config.testDb, {}, err => {
+  if (err) return console.log(err);
+  console.log('Test DB connection successful!!');
+});
+
+chai.use(chaiHTTP);
+
+describe('Bands', () => {
+  let bandId;
+  beforeEach(done => {});
+  afterEach(done => {});
+  describe(`[GET] /api/bands`, () => {});
+});
